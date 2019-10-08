fac4aba29cf1baef065e5a8b401051623a299431
alienor1 <alienorpro@gmail.com>
2019-10-08 15:43:54 -0700

    Update files in folder

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "4b6f407bc3690ac1562800a586bbda13"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBH17Xn87x8SM7waCAzHnS4gKMWRX96iDQ"
